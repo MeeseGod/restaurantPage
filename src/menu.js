2ec9a3c0672ce974387db98db0c16e4974495dd9
JosephR <joseph_rundell@outlook.com>
2021-10-09 15:32:56 -0400

    Updated files

--- src/menu.js
+++ src/menu.js
@@ -20,17 +20,71 @@ function generateDivs(){
     for(let i = 0; i < 6; i++){
         let createMenuDiv = document.createElement('div');
         createMenuDiv.setAttribute('id', `menuDiv${i}`);
+        createMenuDiv.setAttribute('class', 'menuDivs');
         document.getElementById('menuGrid').appendChild(createMenuDiv);
     }
 }
 
 function populateMenu(){
     let item1header = document.createElement('h3');
-    item1header.setAttribute('id', 'item1');
+    item1header.setAttribute('id', 'item1header');
     item1header.setAttribute('class', 'menuItem');
     item1header.textContent = "Soylent Shake";
 
+    let item1description = document.createElement('p');
+    item1description.setAttribute('id', 'item1description');
+    item1description.setAttribute('class', 'menuDescription');
+    item1description.textContent = "The classic Soylent Green shake for your meal on-the-go.";
+
     document.getElementById('menuDiv0').appendChild(item1header);
+    document.getElementById('menuDiv0').appendChild(item1description);
+
+    let item2header = document.createElement('h3');
+    item2header.setAttribute('id', 'item2');
+    item2header.setAttribute('class', 'menuItem');
+    item2header.textContent = "Soylent Steak";
+
+    let item2description = document.createElement('p');
+    item2description.setAttribute('id', 'item2description');
+    item2description.setAttribute('class', 'menuDescription');
+    item2description.textContent = "The Soylent Steak is the healthy alternative to the classic natural steak.";
+
+    document.getElementById('menuDiv1').appendChild(item2header);
+    document.getElementById('menuDiv1').appendChild(item2description);
+
+    let item3header = document.createElement('h3');
+    item3header.setAttribute('id', 'item3');
+    item3header.setAttribute('class', 'menuItem');
+    item3header.textContent = "Soylent Stew";
+
+    let item3description = document.createElement('p');
+    item3description.setAttribute('id', 'item3description');
+    item3description.setAttribute('class', 'menuDescription');
+    item3description.textContent = "What's in it? Soylent Green! What else is in it? We don't know and neither does the FDA.";
+
+    document.getElementById('menuDiv2').appendChild(item3header);
+    document.getElementById('menuDiv2').appendChild(item3description);
+
+    let item4header = document.createElement('h3');
+    item4header.setAttribute('id', 'item4');
+    item4header.setAttribute('class', 'menuItem');
+    item4header.textContent = "Soylent Shake";
+
+    document.getElementById('menuDiv3').appendChild(item4header);
+
+    let item5header = document.createElement('h3');
+    item5header.setAttribute('id', 'item5');
+    item5header.setAttribute('class', 'menuItem');
+    item5header.textContent = "Soylent Shake";
+
+    document.getElementById('menuDiv4').appendChild(item5header);
+
+    let item6header = document.createElement('h3');
+    item6header.setAttribute('id', 'item6');
+    item6header.setAttribute('class', 'menuItem');
+    item6header.textContent = "Soylent Shake";
+
+    document.getElementById('menuDiv5').appendChild(item6header);
 }
 
 export default displayMenu;
